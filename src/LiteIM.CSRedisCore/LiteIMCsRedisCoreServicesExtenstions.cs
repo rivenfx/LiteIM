f64b42d0afd423f16ac58d4466a602e4dfb9baa2
staneee <yi.hang@live.com>
2021-06-01 14:08:02 +0800

    增加注释

--- src/LiteIM.CSRedisCore/LiteIMCsRedisCoreServicesExtenstions.cs
+++ src/LiteIM.CSRedisCore/LiteIMCsRedisCoreServicesExtenstions.cs
@@ -9,14 +9,42 @@ namespace LiteIM
 {
     public static class LiteIMCsRedisCoreServicesExtenstions
     {
+        /// <summary>
+        /// 添加 LiteIM CsRedis 到依赖注入容器
+        /// </summary>
+        /// <typeparam name="TImClient">Client类型</typeparam>
+        /// <typeparam name="TImClientOptions">Client配置类型</typeparam>
+        /// <param name="services"></param>
+        /// <param name="options">client 配置</param>
+        /// <returns></returns>
         public static IServiceCollection AddLiteIMCsRedis<TImClient, TImClientOptions>(this IServiceCollection services, TImClientOptions options)
             where TImClient : class, IImClient, new()
             where TImClientOptions : class, ICsRedisCoreImClientOptions, new()
         {
-            services.TryAddSingleton<ICsRedisCoreImClientOptions>(options);
+            services.AddLiteIMCsRedis<TImClient, TImClientOptions>((s) =>
+            {
+                return options;
+            });
 
-            services.AddLiteIMCore<TImClient, TImClientOptions>(options);
-            
+            return services;
+        }
+
+        /// <summary>
+        /// 添加 LiteIM CsRedis 到依赖注入容器
+        /// </summary>
+        /// <typeparam name="TImClient">Client类型</typeparam>
+        /// <typeparam name="TImClientOptions">Client配置类型</typeparam>
+        /// <param name="services"></param>
+        /// <param name="optionConfig">client 配置获取函数</param>
+        /// <returns></returns>
+        public static IServiceCollection AddLiteIMCsRedis<TImClient, TImClientOptions>(this IServiceCollection services, Func<IServiceProvider, TImClientOptions> optionConfig)
+            where TImClient : class, IImClient, new()
+            where TImClientOptions : class, ICsRedisCoreImClientOptions, new()
+        {
+            services.TryAddSingleton<ICsRedisCoreImClientOptions>(optionConfig);
+
+            services.AddLiteIMCore<TImClient, TImClientOptions>(optionConfig);
+
             return services;
         }
     }
--- src/LiteIM.Core/LiteIMServicesExtenstions.cs
+++ src/LiteIM.Core/LiteIMServicesExtenstions.cs
@@ -9,11 +9,38 @@ namespace LiteIM
 {
     public static class LiteIMServicesExtenstions
     {
+        /// <summary>
+        /// 添加 LiteIM 到依赖注入容器
+        /// </summary>
+        /// <typeparam name="TImClient">Client类型</typeparam>
+        /// <typeparam name="TImClientOptions">Client配置类型</typeparam>
+        /// <param name="services"></param>
+        /// <param name="options">client 配置</param>
+        /// <returns></returns>
         public static IServiceCollection AddLiteIMCore<TImClient, TImClientOptions>(this IServiceCollection services, TImClientOptions options)
             where TImClient : class, IImClient, new()
             where TImClientOptions : class, IImClientOptions, new()
         {
-            services.TryAddSingleton<IImClientOptions>(options);
+            services.AddLiteIMCore<TImClient, TImClientOptions>((s) =>
+            {
+                return options;
+            });
+            return services;
+        }
+
+        /// <summary>
+        /// 添加 LiteIM 到依赖注入容器
+        /// </summary>
+        /// <typeparam name="TImClient">Client类型</typeparam>
+        /// <typeparam name="TImClientOptions">Client配置类型</typeparam>
+        /// <param name="services"></param>
+        /// <param name="optionConfig">client 配置获取函数</param>
+        /// <returns></returns>
+        public static IServiceCollection AddLiteIMCore<TImClient, TImClientOptions>(this IServiceCollection services, Func<IServiceProvider, TImClientOptions> optionConfig)
+           where TImClient : class, IImClient, new()
+           where TImClientOptions : class, IImClientOptions, new()
+        {
+            services.TryAddSingleton<IImClientOptions>(optionConfig);
             services.TryAddSingleton<IImClient, TImClient>();
 
             return services;
