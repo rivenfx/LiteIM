7787006191af8efe4402ce574bbed7a735e647f2
staneee <yi.hang@live.com>
2021-06-04 10:33:09 +0800

    修改注释

--- src/LiteIM.Core/IImClient.cs
+++ src/LiteIM.Core/IImClient.cs
@@ -21,51 +21,50 @@ namespace LiteIM
         bool HasOnline(string clientId);
 
 
-        #region 群聊频道
+        #region 频道
 
         /// <summary>
-        /// 加入群聊频道，每次上线都必须重新加入
+        /// 加入频道，每次上线都必须重新加入
         /// </summary>
         /// <param name="clientId">客户端id</param>
-        /// <param name="chan">群聊频道名</param>
+        /// <param name="chan">频道名</param>
         void JoinChan(string clientId, string chan);
 
         /// <summary>
-        /// 离开群聊频道
+        /// 离开频道
         /// </summary>
         /// <param name="clientId">客户端id</param>
-        /// <param name="chans">群聊频道名</param>
+        /// <param name="chans">频道名</param>
         void LeaveChan(string clientId, params string[] chans);
 
         /// <summary>
-        /// 离开所有群聊频道
+        /// 离开所有频道
         /// </summary>
         /// <param name="clientId">客户端id</param>
         void LeaveChan(string clientId);
 
         /// <summary>
-        /// 获取群聊频道所有客户端id（测试）
+        /// 获取频道所有客户端id（测试）
         /// </summary>
-        /// <param name="chan">群聊频道名</param>
+        /// <param name="chan">频道名</param>
         /// <returns></returns>
         IEnumerable<string> GetChanClientList(string chan);
 
-
         /// <summary>
-        /// 清理群聊频道的离线客户端（测试）
+        /// 清理频道的离线客户端（测试）
         /// </summary>
-        /// <param name="chan">群聊频道名</param>
+        /// <param name="chan">频道名</param>
         void ClearChanClient(string chan);
 
 
         /// <summary>
-        /// 获取所有群聊频道和在线人数
+        /// 获取所有频道和在线人数
         /// </summary>
         /// <returns>频道名和在线人数</returns>
         IEnumerable<(string chan, long online)> GetChanList();
 
         /// <summary>
-        /// 获取用户参与的所有群聊频道
+        /// 获取用户参与的所有频道
         /// </summary>
         /// <param name="clientId">客户端id</param>
         /// <returns></returns>
@@ -73,9 +72,9 @@ namespace LiteIM
 
 
         /// <summary>
-        /// 获取群聊频道的在线人数
+        /// 获取频道的在线人数
         /// </summary>
-        /// <param name="chan">群聊频道名</param>
+        /// <param name="chan">频道名</param>
         /// <returns>在线人数</returns>
         long GetChanOnline(string chan);
 
