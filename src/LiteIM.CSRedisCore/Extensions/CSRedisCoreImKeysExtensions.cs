b6224cf553dc6a3a5a6d52b2a4ef02d7bd325832
stan e <yi.hang@live.com>
2022-12-03 12:15:29 +0800

    增加注释，修改redis版本号为3.8.3

--- src/LiteIM.CSRedisCore/Extensions/CSRedisCoreImKeysExtensions.cs
+++ src/LiteIM.CSRedisCore/Extensions/CSRedisCoreImKeysExtensions.cs
@@ -6,26 +6,53 @@ namespace LiteIM.Extensions
 {
     public static class CSRedisCoreImKeysExtensions
     {
+        /// <summary>
+        /// 缓存键-频道
+        /// </summary>
+        /// <param name="prefix"></param>
+        /// <param name="end"></param>
+        /// <returns></returns>
         public static string CSRedisCoreChan(this string prefix, string end)
         {
             return $"n:{prefix}LiteIM,c:{string.Empty.Chan(end)}";
         }
 
+        /// <summary>
+        /// 缓存键-客户端
+        /// </summary>
+        /// <param name="prefix"></param>
+        /// <param name="end"></param>
+        /// <returns></returns>
         public static string CSRedisCoreClient(this string prefix, string end)
         {
             return $"n:{prefix}LiteIM,c:{string.Empty.Client(end)}";
         }
 
+        /// <summary>
+        /// 缓存键-频道列表
+        /// </summary>
+        /// <param name="prefix"></param>
+        /// <returns></returns>
         public static string CSRedisCoreListChan(this string prefix)
         {
             return $"n:{prefix}LiteIM,c:{string.Empty.ListChan()}";
         }
 
+        /// <summary>
+        /// 缓存键-在线客户端
+        /// </summary>
+        /// <param name="prefix"></param>
+        /// <returns></returns>
         public static string CSRedisCoreOnline(this string prefix)
         {
             return $"n:{prefix}LiteIM,c:{string.Empty.Online()}";
         }
 
+        /// <summary>
+        /// 缓存键-离线客户端
+        /// </summary>
+        /// <param name="prefix"></param>
+        /// <returns></returns>
         public static string CSRedisCoreOffline(this string prefix)
         {
             return $"n:{prefix}LiteIM,c:{string.Empty.Offline()}";
--- src/LiteIM.Core/Extensions/ImKeysExtensions.cs
+++ src/LiteIM.Core/Extensions/ImKeysExtensions.cs
@@ -6,26 +6,53 @@ namespace LiteIM.Extensions
 {
     public static class ImKeysExtensions
     {
+        /// <summary>
+        /// 缓存键-频道
+        /// </summary>
+        /// <param name="prefix"></param>
+        /// <param name="end"></param>
+        /// <returns></returns>
         public static string Chan(this string prefix, string end)
         {
             return $"{prefix}{ImConsts.Chan}_{end}";
         }
 
+        /// <summary>
+        /// 缓存键-客户端
+        /// </summary>
+        /// <param name="prefix"></param>
+        /// <param name="end"></param>
+        /// <returns></returns>
         public static string Client(this string prefix, string end)
         {
             return $"{prefix}{ImConsts.Client}_{end}";
         }
 
+        /// <summary>
+        /// 缓存键-频道列表
+        /// </summary>
+        /// <param name="prefix"></param>
+        /// <returns></returns>
         public static string ListChan(this string prefix)
         {
             return $"{prefix}{ImConsts.ListChan}";
         }
 
+        /// <summary>
+        /// 缓存键-在线客户端
+        /// </summary>
+        /// <param name="prefix"></param>
+        /// <returns></returns>
         public static string Online(this string prefix)
         {
             return $"{prefix}{ImConsts.Online}";
         }
 
+        /// <summary>
+        /// 缓存键-离线客户端
+        /// </summary>
+        /// <param name="prefix"></param>
+        /// <returns></returns>
         public static string Offline(this string prefix)
         {
             return $"{prefix}{ImConsts.Offline}";
